0cab2932499083221dbb3064dd573e3af6f31daa
Anton Blomberg <ablomberg@spotify.com>
2022-11-01 19:21:55 +0100

    Modify to work with Ubuntu 22.04 on Raspberry Pi

---
+++ overlay.sh
@@ -0,0 +1,75 @@
+# Local filesystem mounting			-*- shell-script -*-
+
+#
+# This script overrides local_mount_root() in /usr/share/initramfs-tools/scripts/local
+# and mounts root as a read-only filesystem with a temporary (rw)
+# overlay filesystem.
+#
+
+. /scripts/local
+
+local_mount_root()
+{
+	local_top
+	if [ -z "${ROOT}" ]; then
+		panic "No root device specified. Boot arguments must include a root= parameter."
+	fi
+
+	local_device_setup "${ROOT}" "root file system"
+	ROOT="${DEV}"
+
+	# Get the root filesystem type if not set
+	if [ -z "${ROOTFSTYPE}" ] || [ "${ROOTFSTYPE}" = auto ]; then
+		FSTYPE=$(get_fstype "${ROOT}")
+	else
+		FSTYPE=${ROOTFSTYPE}
+	fi
+
+	local_premount
+
+	# CHANGES TO THE ORIGINAL FUNCTION BEGIN HERE
+	# N.B. this code still lacks error checking
+
+  # TODO: If the /etc/fstab change is commented out in overlayfs.sh, then /boot/firmware will still be R/W (may be a good thing)
+
+	modprobe ${FSTYPE}
+	checkfs "${ROOT}" root "${FSTYPE}"
+
+	# Create directories for root and the overlay
+	mkdir /lower /upper
+
+	# Create a folder for persistent data. It will be mounted as R/W
+	mkdir -p ${rootmnt}/persistent
+
+	# Mount read-only root to /lower
+	if [ "${FSTYPE}" != "unknown" ]; then
+		mount ${FSTYPE:+-t "${FSTYPE}"} ${ROOTFLAGS} "${ROOT}" /lower
+		mkdir -p /lower/persistent
+
+		# Remount with read only (-r). For some reason mount -o remount,r returned "Invalid argument"
+		umount /lower
+		mount -r ${FSTYPE:+-t "${FSTYPE}"} ${ROOTFLAGS} "${ROOT}" /lower
+	else
+		mount ${ROOTFLAGS} ${ROOT} /lower
+		mkdir -p /lower/persistent
+
+    # Remount with read only (-r). For some reason mount -o remount,r returned "Invalid argument"
+		umount /lower
+		mount -r ${ROOTFLAGS} ${ROOT} /lower
+	fi
+
+	modprobe overlay
+
+	# Mount a tmpfs for the overlay in /upper
+	mount -t tmpfs tmpfs /upper
+	mkdir /upper/data /upper/work
+
+	# Mount the final overlay-root in $rootmnt
+	mount -t overlay \
+	    -olowerdir=/lower,upperdir=/upper/data,workdir=/upper/work \
+	    overlay ${rootmnt}
+
+	# Make /persistent R/W
+	mount -o bind /lower/persistent ${rootmnt}/persistent
+	mount -o remount,rw ${rootmnt}/persistent
+}
--- overlayfs.sh
+++ overlayfs.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
+set -e
+set -o pipefail
 
+# Please upgrade the kernel before continuing:
+# apt update && apt upgrade && reboot now
 apt install initramfs-tools
 
+# Configure update-initramfs to include the overlay module
 if ! grep overlay /etc/initramfs-tools/modules > /dev/null; then
   echo overlay >> /etc/initramfs-tools/modules
 fi
 
-cp overlay /etc/initramfs-tools/scripts
+# Install the overlay script which configures mounts on boot
+# Note: Every time this file is changed the initrd image needs to be recreated with update-initramfs
+cp overlay.sh /etc/initramfs-tools/scripts/overlay
 
-update-initramfs -c -k $(uname -r)
-mv /boot/initrd.img-$(uname -r) /boot/initrd7.img
+# Backup current image
+cp "/boot/initrd.img-$(uname -r)" "/boot/initrd.img-$(uname -r).bak"
 
-sed -e "s/initramfs.*//" -i /boot/config.txt
-echo initramfs initrd7.img >> /boot/config.txt
+# Create a new image with the overlay module included
+update-initramfs -c -k "$(uname -r)"
 
-cp /boot/cmdline.txt /boot/cmdline.txt.orig
-sed -e "s/\(.*\)/boot=overlay \1/" -i /boot/cmdline.txt
-cp /boot/cmdline.txt /boot/cmdline.txt.overlay
+# TODO: Useful commands for debugging
+# sudo update-initramfs -c -k "$(uname -r)" && sudo reboot now
+# sudo less /etc/initramfs-tools/scripts/overlay
+# sudo less /run/initramfs/initramfs.debug
 
+# Backup current cmdline.txt
+cp /boot/firmware/cmdline.txt /boot/firmware/cmdline.txt.bak
+
+# Make a copy of cmdline.txt without boot=overlay, add boot=overlay to the copy, and replace cmdline.txt with the copy
+sed -e 's/.*/ & /;:1;s/ boot=overlay / /g;t1;s/ \+/ /g;s/^ //;s/ $//' "/boot/firmware/cmdline.txt" > "/boot/firmware/cmdline.txt.orig" &&
+sed -e "s/.*/boot=overlay &/" "/boot/firmware/cmdline.txt.orig" > "/boot/firmware/cmdline.txt.overlay" &&
+cp "/boot/firmware/cmdline.txt.overlay" "/boot/firmware/cmdline.txt"
+
+# Install utility script
 cp overctl /usr/local/sbin
 
-sed -e "s/\(.*\/boot.*\)defaults\(.*\)/\1defaults,ro\2/" -i /etc/fstab
+# TODO: Do I actually need this? Probably not
+# Backup current fstab
+#cp /etc/fstab /etc/fstab.bak
+# Make /boot/firmware readonly
+#sed -e "s/\(.*\/boot.*\)defaults\(.*\)/\1defaults,ro\2/" -i /etc/fstab
